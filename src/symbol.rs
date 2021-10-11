731d327d3dec37c439ba9e0014e5019ed866a9f5
Yuto Watanabe <yuto.w51942@gmail.com>
2021-10-11 21:03:10 +0900

    changed symnbol

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,40 +1,100 @@
-pub fn is_start_brace(target: &str) -> bool {
-    comparison_symbol(target, "{")
+/// Check if the character is `{`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_start_brace(target: &u8) -> bool {
+    // `{` is 123
+    comparison_symbol(target, 123u8)
 }
 
-pub fn is_end_brace(target: &str) -> bool {
-    comparison_symbol(target, "}")
+/// Check if the character is `}`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_end_brace(target: &u8) -> bool {
+    // `}` is 125
+    comparison_symbol(target, 125u8)
 }
 
-pub fn is_start_bracket(target: &str) -> bool {
-    comparison_symbol(target, "[")
+/// Check if the character is `[`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_start_bracket(target: &u8) -> bool {
+    // `[` is 91
+    comparison_symbol(target, 91u8)
 }
 
-pub fn is_end_bracket(target: &str) -> bool {
-    comparison_symbol(target, "]")
+/// Check if the character is `}`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_end_bracket(target: &u8) -> bool {
+    // `]` is 93
+    comparison_symbol(target, 93u8)
 }
 
-pub fn is_comma(target: &str) -> bool {
-    comparison_symbol(target, ",")
+/// Check if the character is `,`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_comma(target: &u8) -> bool {
+    // `,` is 44
+    comparison_symbol(target, 44u8)
 }
 
-pub fn is_colon(target: &str) -> bool {
-    comparison_symbol(target, ":")
+/// Check if the character is `:`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_colon(target: &u8) -> bool {
+    // `:` is 58
+    comparison_symbol(target, 58u8)
 }
 
-pub fn is_double_quotation(target: &str) -> bool {
+/// Check if the character is `"`.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_double_quotation(target: &u8) -> bool {
     comparison_symbol(target, "\"")
 }
 
-pub fn is_blank(target: &str) -> bool {
-    comparison_symbol(target, " ")
+/// Check if the character is whitespace.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_blank(target: &u8) -> bool {
+    // whitespace is 32
+    comparison_symbol(target, 32u8)
 }
 
-pub fn is_next(target: &str) -> bool {
-    comparison_symbol(target, "\n")
+/// Check if the character is newline.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+pub fn is_next(target: &u8) -> bool {
+    // newline(\n) is 10
+    comparison_symbol(target, 10u8)
 }
 
-fn comparison_symbol(target: &str, symbol: &str) -> bool {
+/// Compares two types of UTF-8 characters.
+/// True if they are the same. If not, it returns false.
+///
+/// ## Arguments
+///
+/// - `target` - target string.
+/// - `symbol` - string to be compared.
+fn comparison_symbol(target: &u8, symbol: &u8) -> bool {
     match target {
         symbol => true,
         _ => false,
